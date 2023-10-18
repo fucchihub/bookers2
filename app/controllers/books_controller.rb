f6610c64f467cbc0c60a9caab7982e519f3f7b47
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-10-18 13:06:23 +0000

    [Progress]あと部分テンプレとBootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,19 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "Your book has been successfully created !"
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -15,15 +21,27 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Your book has been successfully updated !"
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path(@book)
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,30 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
-  def index
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your profile has been successfully updated !"
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
